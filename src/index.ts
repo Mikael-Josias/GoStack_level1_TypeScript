9d617592ae40e4bf23906b7bb45d370b04ce6d38
Mikael Josias <mikaeljosias2002@gmail.com>
2020-11-30 13:32:36 -0300

    feat(routes): create a function that returns hello message

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import express, { response } from 'express';
+import express from 'express';
+import { helloWorld } from './routes';
+
+/**
+ * A importação do express está dentro do mesmo
+ * aqruivo que suas funcionalidades estão sem usadas.
+ * Portanto não é nescessario informar as tipagens.
+ */
 
 const app = express();
 
-app.get('/', (request, reponse) =>
-{
-    return response.json({message: 'Hello World'})
-});
+app.get('/', helloWorld);
 
 app.listen(3333);
 
